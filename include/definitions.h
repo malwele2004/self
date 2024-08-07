6b94f749046f1b7ba8cd8d36619200ef805aea32
malwele2004 <malwele085@gmail.com>
2024-08-08 00:15:52 +0200

    Update definitions.h

--- include/definitions.h
+++ include/definitions.h
@@ -1,1 +1,21 @@
 
+#ifndef DEF
+#define DEF
+
+struct memory {
+  char* ptr;
+  long allocated, 
+		   write_ptr;
+};
+
+struct block 
+{
+		struct block *parent,
+								 *handler,
+								 *chandler,
+								 *children;
+                 
+    long nchildren;
+		struct memory data;
+}; 
+#endif
